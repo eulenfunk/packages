eefc7440788b3a4b786c2e0a6fe350ea4b303142
Adorfer <adorferen@gmail.com>
2016-02-02 01:32:22 +0100

    fix metrics
    improvement against other adhoc meshes

--- gluon-wificheck/files/lib/gluon/wificheck/wificheck.sh
+++ gluon-wificheck/files/lib/gluon/wificheck/wificheck.sh
@@ -4,18 +4,23 @@ if [ -z "$mname" ] ; then
   exit 0
  else
   echo radio: $mname
-  mesh=$(batctl o|grep $mname|wc -l)
+  mesh=$(batctl o|grep $mname|wc -l) # Keine=1!
   wmesh=$(iw dev $mname scan|grep $mname|wc -l)
-  echo meshneighbours: $mesh wifineighbours: $wmesh
+  bssid=$(uci get wireless.ibss_radio0.bssid)
+  neighbors=$(iw dev $mname scan|grep $bssid|wc -l)
+  echo meshneighbours: $mesh adhocneighbors: $wmesh wifimeshneighbors: $neighbors
   if [ ! -f /tmp/noisland ] ; then
-    if [ "$mwesh" != "0" ] ; then
+    if [ "$neighbors" -gt 2 ] ; then #minimum 2 neighbors
       echo 1>/tmp/noisland
     fi
    else
-    if [ "$wmesh" == "0" ] ; then
+    if [ "$neighbors" -lt 2 ] ; then
       if [ -f /tmp/wifipbflag ] ; then
+        echo "still no wifi neighbors, rebooting"
+        sleep 3
         reboot -f
        else
+        echo "lost wifi neighbors."
         echo 1>/tmp/wifipbflag
        fi
     else
