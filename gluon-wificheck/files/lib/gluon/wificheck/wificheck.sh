743ec057426428f9b83905787f6b79b7d65ba1f5
Adorfer <adorferen@gmail.com>
2016-02-03 16:26:54 +0100

    filter for direct batman-ibss-originators

--- gluon-wificheck/files/lib/gluon/wificheck/wificheck.sh
+++ gluon-wificheck/files/lib/gluon/wificheck/wificheck.sh
@@ -4,11 +4,11 @@ if [ -z "$mname" ] ; then
   exit 0
  else
   echo radio: $mname
-  mesh=$(batctl o|grep $mname|wc -l) # Keine=1!
+  mesh=$(batctl o|grep ibss0|cut -d")"  -f 2|cut -d" " -f 2|grep [.?.?:.?.?:.*]|sort|uniq|wc -l)
   wmesh=$(iw dev $mname scan|grep $mname|wc -l)
   bssid=$(uci get wireless.ibss_radio0.bssid)
   neighbors=$(iw dev $mname scan|grep $bssid|wc -l)
-  echo meshneighbours: $mesh adhocneighbors: $wmesh wifimeshneighbors: $neighbors
+  echo ibss-bat-neighbours: $mesh wifiadhocs-neighbors: $wmesh wifimesh-neighbors: $neighbors
   if [ ! -f /tmp/noisland ] ; then
     if [ "$mesh" -gt 1 ] ; then #minimum 2 neighbors
       echo 1>/tmp/noisland
