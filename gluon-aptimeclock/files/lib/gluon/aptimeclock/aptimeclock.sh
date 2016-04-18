a1f202a4e3dba6c120853c73a91e8850f3080ace
Adorfer <adorferen@gmail.com>
2016-04-18 23:47:44 +0200

    fix to make make changes permanent.

--- gluon-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
+++ gluon-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
@@ -14,10 +14,10 @@ CurrentDayOfWeek="$(date +%w)"
 
 dummy=$(uci get wireless.client_radio0.disabled)
 if [ $? -eq 0 ]; then
-  dummy=$(uci get wireless.client_radio0.clock_on)
+  dummy=$(uci get wireless.radio0.client_clock_on)
   if [ $? -eq 0 ]; then
-    apclock0on=$(uci get wireless.client_radio0.clock_on)
-    apclock0off=$(uci get wireless.client_radio0.clock_off)
+    apclock0on=$(uci get wireless.radio0.client_clock_on)
+    apclock0off=$(uci get wireless.radio0.client_clock_off)
     if ( [ ${#apclock0on} -eq 4 ] ) && ( [ ${#apclock0off} -eq 4 ] ) ; then
       if ( ( ( [ $apclock0on -le $apclock0off ] ) && ( ( [ $CurrentTime -le $apclock0on ] ) || ( [ $CurrentTime -ge $apclock0off ] ) ) ) || ( ( [ $apclock0on -ge $apclock0off ] ) && ( ( [ $CurrentTime -le $apclock0on ] ) && ( [ $CurrentTime -ge $apclock0off ] ) ) ) ) ; then
         if [ $(uci get wireless.client_radio0.disabled) -eq 0 ] ; then
@@ -44,10 +44,10 @@ if [ $? -eq 0 ]; then
 
 dummy=$(uci get wireless.client_radio1.disabled)
 if [ $? -eq 0 ]; then
-  dummy=$(uci get wireless.client_radio1.clock_on)
+  dummy=$(uci get wireless.radio1.client_clock_on)
   if [ $? -eq 0 ]; then
-    apclock1on=$(uci get wireless.client_radio1.clock_on)
-    apclock1off=$(uci get wireless.client_radio1.clock_off)
+    apclock1on=$(uci get wireless.radio1.client_clock_on)
+    apclock1off=$(uci get wireless.radio1.client_clock_off)
     if ( [ ${#apclock1on} -eq 4 ] ) && ( [ ${#apclock1off} -eq 4 ] ) ; then
       if ( ( ( [ $apclock1on -le $apclock1off ] ) && ( ( [ $CurrentTime -le $apclock1on ] ) || ( [ $CurrentTime -ge $apclock1off ] ) ) ) || ( ( [ $apclock1on -ge $apclock1off ] ) && ( ( [ $CurrentTime -le $apclock1on ] ) && ( [ $CurrentTime -ge $apclock1off ] ) ) ) ) ; then
         if [ $(uci get wireless.client_radio1.disabled) -eq 0 ] ; then
