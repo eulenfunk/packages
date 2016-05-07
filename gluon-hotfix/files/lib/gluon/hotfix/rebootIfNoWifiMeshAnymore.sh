875d4dcb050f9b7aa2ded98fffc8d6567111066a
Adorfer <adorferen@gmail.com>
2016-05-07 03:38:12 +0200

    -typo

--- gluon-hotfix/files/lib/gluon/hotfix/rebootIfNoWifiMeshAnymore.sh
+++ gluon-hotfix/files/lib/gluon/hotfix/rebootIfNoWifiMeshAnymore.sh
@@ -12,7 +12,6 @@ r0ibss=$(uci get wireless.ibss_radio0.disabled)
 r1ibss=$(uci get wireless.ibss_radio1.disabled)
 
 if [ $r0mesh = '0' ] | [ $r0mesh = '0' ] | [ $r0mesh = '0' ] | [ $r0mesh = '0' ] ; then
-  wireless.mesh_radio0.disabled='0'
   batctl o | grep -q "ibss0\|mesh0"
   if [ $? == 0 ] ; then
     #found wifi-mesh on mesh0*
