8982e053aac2e37b43de9a2ed0497521990902f9
Milan Pssler <me@petabyteboy.de>
2016-06-13 20:17:59 +0200

    only use gw and anycast check if it succeeded once

--- gluon-hotfix/files/lib/gluon/hotfix/rebootIfNoGw.sh
+++ gluon-hotfix/files/lib/gluon/hotfix/rebootIfNoGw.sh
@@ -5,18 +5,21 @@ upgrade_started='/tmp/autoupdate.lock'
 
 batctl gwl | grep -q "No gateways in range"
 if [ $? == 0 ] ; then
-  if [ -f /tmp/nogwflag.3 ] ; then
-    [ -f $upgrade_started ] && exit
-    securereboot
-  elif [ -f /tmp/nogwflag.2 ] ; then
-    touch /tmp/nogwflag.3
-  elif [ -f /tmp/nogwflag.1 ] ; then
-    touch /tmp/nogwlflag.2
-  else
-    touch /tmp/nogwflag.1
+  if [ -f /tmp/gw ] ; then
+    if [ -f /tmp/gwgone.3 ] ; then
+      [ -f $upgrade_started ] && exit
+      securereboot
+    elif [ -f /tmp/gwgone.2 ] ; then
+      touch /tmp/gwgone.3
+    elif [ -f /tmp/gwgone.1 ] ; then
+      touch /tmp/gwgone.2
+    else
+      touch /tmp/gwgone.1
+    fi
   fi
 else
-  rm -f /tmp/nogwflag.* 2>/dev/null
+  touch /tmp/gw
+  rm -f /tmp/gwgone.* 2>/dev/null
 fi
 
 ipv6_subnet="$(lua -e 'print(require("gluon.site_config").prefix6)' | sed -e 's/\/64//')"
@@ -26,21 +29,24 @@ if [ ! -z "$ipv6_subnet" ]; then
   returnval="$?"
 fi
 if [ "$returnval" -ne 0 ] || [ -z "$ipv6_subnet" ]; then
-  logger "IPv6 Anycast-IP NOT reachable."
-  if [ -f /tmp/noip6routerflag.3 ] ; then
-    [ -f $upgrade_started ] && exit
-    securereboot
-    exit 0
-  elif [ -f /tmp/noip6routerflag.2 ] ; then
-    touch /tmp/noip6routerflag.3
-  elif [ -f /tmp/noip6routerflag.1 ] ; then
-    touch /tmp/noip6routerflag.2
-  else
-    touch /tmp/noip6routerflag.1
+  if [ -f /tmp/ip6anycast ] ; then
+    logger "IPv6 Anycast-IP NOT reachable."
+    if [ -f /tmp/ip6anycastgone.3 ] ; then
+      [ -f $upgrade_started ] && exit
+      securereboot
+      exit 0
+    elif [ -f /tmp/ip6anycastgone.3 ] ; then
+      touch /tmp/ip6anycastgone.3
+    elif [ -f /tmp/ip6anycastgone.1 ] ; then
+      touch /tmp/ip6anycastgone.2
+    else
+      touch /tmp/ip6anycastgone.1
+    fi
   fi
 else
   logger "IPv6 Anycast-IP reachable."
-  rm -f /tmp/noip6routerflag.* 2>/dev/null
+  touch /tmp/ip6anycast
+  rm -f /tmp/ip6anycastgone.* 2>/dev/null
 fi
 
 #check if wifi is stucking
