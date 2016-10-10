f949494fede800b090b9358fb3252e75cb6318d5
Milan Paessler <me@petabyteboy.de>
2016-10-10 12:41:04 +0200

    add gluon-quickfix, v2016.2

---
+++ gluon-quickfix/files/lib/gluon/quickfix/quickfix.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#################
+# safety checks #
+#################
+
+# if autoupdater is running, exit
+pgrep -f autoupdater && exit 0
+
+# if the router started less than 5 minutes ago, exit
+[ $(cat /proc/uptime | sed 's/\..*//g') -gt 300 ] || exit 0
+
+echo safety checks done, continuing...
+
+#########
+# fixes #
+#########
+
+# fix dead mesh
+[ $(iw dev mesh0 station dump | wc -l) -eq 0 ] && iw dev mesh0 scan
+
+###########
+# reboots #
+###########
+
+# if respondd not running, reboot (probably ram was full, so more services might've crashed)
+pgrep respondd || reboot
+
