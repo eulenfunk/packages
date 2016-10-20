d88f924ed294c94a0a420ba696f0e74513f04979
Milan Paessler <me@petabyteboy.de>
2016-10-20 15:28:56 +0200

    better conditions for iw scan fix and gluon #910 workaround

--- gluon-quickfix/files/lib/gluon/quickfix/quickfix.sh
+++ gluon-quickfix/files/lib/gluon/quickfix/quickfix.sh
@@ -16,8 +16,20 @@ echo safety checks done, continuing...
 # fixes #
 #########
 
-# fix dead mesh
-[ $(iw dev mesh0 station dump | wc -l) -eq 0 ] && iw dev mesh0 scan && logger -s -t "gluon-quickfix" -p 5 "no neighbours, running scan"
+function scan {
+		iw dev mesh0 scan >/dev/null
+		logger -s -t "gluon-quickfix" -p 5 "neighbour lost, running iw scan"
+}
+
+OLD_NEIGHBOURS=$(cat /tmp/neighbours_mesh0 2>/dev/null)
+NEIGHBOURS=$(iw dev mesh0 station dump | grep -e "^Station " | awk '{ print $2 }')
+echo $NEIGHBOURS > /tmp/neighbours_mesh0
+
+# check if we have lost any neighbours
+for NEIGHBOUR in $OLD_NEIGHBOURS
+do
+	echo $NEIGHBOURS | grep $NEIGHBOUR >/dev/null || (scan; break)
+done
 
 ###########
 # reboots #
@@ -26,4 +38,8 @@ echo safety checks done, continuing...
 # if respondd or dropbear not running, reboot (probably ram was full, so more services might've crashed)
 pgrep respondd || reboot
 pgrep dropbear || reboot
+
+# reboot if there was a kernel (batman) error
+# for an example gluon issue #910
+dmesg | grep -i "kernel bug" && reboot
 
