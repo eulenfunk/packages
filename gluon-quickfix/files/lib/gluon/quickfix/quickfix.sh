d63dc8b76cea6f2e0665a97f7832c41aad136869
Adorfer <adorferen@gmail.com>
2017-05-28 05:27:10 +0200

    adding autoupdater lockfile

--- gluon-quickfix/files/lib/gluon/quickfix/quickfix.sh
+++ gluon-quickfix/files/lib/gluon/quickfix/quickfix.sh
@@ -1,41 +1,51 @@
 #!/bin/sh
 
-UPGRADESTARTED='/tmp/autoupdate.lock'
-
 safety_exit() {
         echo safety checks failed, exiting with error code 2
         exit 2
 }
 
+now_reboot() {
+        logger -s -t "gluon-quickfix" -p 5 "rebooting... reason: $@"
+        # push log to server here (nyi)
+        # only reboot if the router started less than 1 hour ago
+        if [ "$(cat /proc/uptime | sed 's/\..*//g')" -gt "3600" ] ; then
+          echo rebooting
+          /sbin/reboot -f
+         fi
+        logger -s -t "gluon-quickfix" -p 5 "AprilApril! Nicht während der ersten 60 Minuten nach dem Boot!"
+}
+
+
 # if the router started less than 10 minutes ago, exit
-[ $(cat /proc/uptime | sed 's/\..*//g') -gt 600 ] || safety_exit
+[ "$(cat /proc/uptime | sed 's/\..*//g')" -gt "600" ] || safety_exit
 
 # if autoupdater is running less than 60 minutes, exit. otherwise emergency-reboot
+UPGRADESTARTED='/tmp/autoupdate.lock'
 if [ -f $UPGRADESTARTED ] ; then
   UPDATEWAIT='60'
   MAXAGE=$(($(date +%s)-60*${UPDATEWAIT}))
   LOCKAGE=$(date -r /tmp/autoupdate.lock +%s)
   if [ "$MAXAGE" -gt "$LOCKAGE" ] ; then
-    _reboot "stale autoupdate.lock file"
+    now_reboot "stale autoupdate.lock file"
    fi
   safety_exit
  fi
-
+  
 echo safety checks done, continuing...
 
 # reboot if there was a kernel (batman) error
 # for an example gluon issue #680
-dmesg | grep "Kernel bug" >/dev/null && _reboot "gluon issue #680"
+dmesg | grep "Kernel bug" >/dev/null && now_reboot "gluon issue #680"
 
-if [ "$(uci get wireless.radio0)" eq "wifi-device" ] && [ "$(uci show|grep wireless.radio0.disabled|cut -d"=" -f2)|sed -e "s/r0'1'/OFF/") -ne "OFF" ]  ; then
+if [ "$(uci get wireless.radio0)" == "wifi-device" ] && [ "$(uci show|grep wireless.radio0.disabled|cut -d\"=\" -f2)|sed -e \"s/r0'1'/OFF/\")" != "OFF" ]  ; then
   echo has wifi enabled
   # check for hanging iw
   [ -f /tmp/iwdev.log ] && rm /tmp/iwdev.log
   iw dev>/tmp/iwdev.log &
   sleep 20
-  [ $(cat/tmp/iwdev.log|wc -l) -eq 0 ] && _reboot "iw dev freezes"
+  [ $(cat/tmp/iwdev.log|wc -l) -eq 0 ] && now_reboot "iw dev freezes"
  fi
-
 
 DEV="$(iw dev|grep Interface|grep -e 'mesh0' -e 'ibss0'| awk '{ print $2 }'|head -1)"
 
@@ -54,17 +64,5 @@ do
         echo $NEIGHBOURS | grep $NEIGHBOUR >/dev/null || (scan; break)
 done
 
-###########
-# reboots #
-###########
-
-_reboot() {
-        logger -s -t "gluon-quickfix" -p 5 "rebooting... reason: $@"
-        # push log to server here (nyi)
-        # only reboot if the router started less than 1 hour ago
-        [ $(cat /proc/uptime | sed 's/\..*//g') -gt 1800 ] || /sbin/reboot -f # comment out for debugging purposes
-}
-
-# if respondd or dropbear not running, reboot (probably ram was full, so more services might've crashed)
-pgrep respondd >/dev/null || _reboot "respondd not running"
-pgrep dropbear >/dev/null || _reboot "dropbear not running"
+pgrep respondd >/dev/null || now_reboot "respondd not running"
+pgrep dropbear >/dev/null || now_reboot "dropbear not running"
