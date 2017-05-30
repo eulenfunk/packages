68a3a3b3a6a1a01dfeb80b95b43c438706658d0e
Adorfer <adorferen@gmail.com>
2017-05-30 12:39:16 +0200

    lockfile nur in Verbindung mit dem gluon-quickfix

--- gluon-weeklyreboot/files/lib/gluon/weeklyreboot/weeklyreboot.sh
+++ gluon-weeklyreboot/files/lib/gluon/weeklyreboot/weeklyreboot.sh
@@ -4,5 +4,10 @@ logger -s -t "gluon-weeklyreboot" -p 5 "sheduled reboot in $DELAYTIME seconds"
 sleep $DELAYTIME
 logger -s -t "gluon-weeklyreboot" -p 5 "sheduled reboot in 5 seconds"
 sleep 5
+# Autoupdate? 
+upgrade_started='/tmp/autoupdate.lock'
+if [ -f $upgrade_started ] ; then
+  logger -s -t "gluon-weeklyreboot" -p 5 "Autoupdate läuft! Aborting"
+  exit 2
+ fi
 reboot
-
