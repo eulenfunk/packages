96a85645dfdb6734d9308bd31fd32fabc73e64f1
Adorfer <adorferen@gmail.com>
2019-07-17 04:20:55 +0200

    updated powerfix to wirk with singleradio

--- gluon-txpowerfix/files/lib/gluon/txpowerfix/txpowerfix.lua
+++ gluon-txpowerfix/files/lib/gluon/txpowerfix/txpowerfix.lua
@@ -19,14 +19,18 @@ if uci:get('wireless', 'radio0', 'hwmode') then
         if chanR0 < '16' then
                 interface24 = 'radio0'
                 chanR1 = uci:get('wireless', 'radio1', 'channel')
-                if chanR1 > '15' then
-                        interface50 = 'radio1'
+                if chanR1 then
+                        if chanR1 > '15' then
+                               interface50 = 'radio1'
+                        end
                 end
         elseif chanR0 > '15'  then
                 interface50 = 'radio0'
                 chanR1 = uci:get('wireless', 'radio1', 'channel')
-                if chanR1 < '16' then
-                        interface24 = 'radio1'
+                if chanR1 then
+                        if chanR1 < '16' then
+                                interface24 = 'radio1'
+                        end
                 end
         else
                 os.exit(0) -- something went wrong
@@ -74,40 +78,41 @@ uci:save('wireless')
 uci:commit('wireless')
 
 --- 5G
-if interface50 == 'radio0' or interface50 == 'radio1' then
-	uci:set('wireless', interface50, 'country', country)
-        VHT = cmd('iwinfo ' .. interface50 .. ' htmodelist|xargs -n 1|grep VHT40')
-	if string.match(VHT, 'VHT40') then
-		uci:set('wireless', interface50, 'htmode', 'VHT40')
-	else 
-		uci:set('wireless', interface50, 'htmode', 'HT20')
-	end
-	--- set values (1st pass)
-	uci:save('wireless')
-	uci:commit('wireless')
-	t = cmd('sleep 2')
-	t = cmd('/sbin/wifi')
-	t = cmd('sleep 8')
-	--- get maximum available power and step
-	t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | tail -n 2 | head -n 1 | awk \'{print $1}\'')
-	maximumTxPowerDb = string.gsub(t, "\n", "")
-	maximumTxPowerDb = tonumber(maximumTxPowerDb)
-	if maximumTxPowerDb < 30 then
-	        t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | wc -l')
-        	maximumTxPower = string.gsub(t, "\n", "")
-	        maximumTxPower = tonumber(maximumTxPower)-1
-	else
-	        t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | grep -n "19 dBm" | cut -f1 -d\':\'')
-	        maximumTxPower = string.gsub(t, "\n", "")
-	        maximumTxPower = tonumber(maximumTxPower)-1
-	end
-	--- set values (2nd pass)
-	uci:set('wireless', interface50, 'txpower', maximumTxPower)
-	uci:save('wireless')
-	uci:commit('wireless')
+if interface50 then
+        if interface50 == 'radio0' or interface50 == 'radio1' then
+                uci:set('wireless', interface50, 'country', country)
+                VHT = cmd('iwinfo ' .. interface50 .. ' htmodelist|xargs -n 1|grep VHT40')
+                if string.match(VHT, 'VHT40') then
+                        uci:set('wireless', interface50, 'htmode', 'VHT40')
+                else
+                        uci:set('wireless', interface50, 'htmode', 'HT20')
+                end
+                --- set values (1st pass)
+                uci:save('wireless')
+                uci:commit('wireless')
+                t = cmd('sleep 2')
+                t = cmd('/sbin/wifi')
+                t = cmd('sleep 8')
+                --- get maximum available power and step
+                t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | tail -n 2 | head -n 1 | awk \'{print $1}\'')
+                maximumTxPowerDb = string.gsub(t, "\n", "")
+                maximumTxPowerDb = tonumber(maximumTxPowerDb)
+                if maximumTxPowerDb < 30 then
+                        t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | wc -l')
+                        maximumTxPower = string.gsub(t, "\n", "")
+                        maximumTxPower = tonumber(maximumTxPower)-1
+                else
+                        t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | grep -n "19 dBm" | cut -f1 -d\':\'')
+                        maximumTxPower = string.gsub(t, "\n", "")
+                        maximumTxPower = tonumber(maximumTxPower)-1
+                end
+                --- set values (2nd pass)
+                uci:set('wireless', interface50, 'txpower', maximumTxPower)
+                uci:save('wireless')
+                uci:commit('wireless')
+        end
 end
 
 t = cmd('sleep 2')
 t = cmd('/sbin/wifi')
 t = cmd('sleep 3')
-
