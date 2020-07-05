02e04419728f64a427a6d993ea8a9cc23cafc6de
Adorfer <adorferen@gmail.com>
2020-07-06 00:27:06 +0200

    don't touch if set to outdoormoddon't touch if set to outdoormodee

--- gluon-txpowerfix/files/lib/gluon/txpowerfix/txpowerfix.lua
+++ gluon-txpowerfix/files/lib/gluon/txpowerfix/txpowerfix.lua
@@ -53,16 +53,6 @@ if uci:get('wireless', 'radio0', 'hwmode') then
 end
 
 --- determine country according to 5G
-if interface50 then
-        channel50 = uci:get('wireless', interface50, 'channel')
-        if channel50 == '32' or channel50 == '68' or channel50 == '169' or channel50 == '173' then
-                country = 'DE'
-        elseif channel50 == '138' or channel50 == '142' or channel50 == '144'  then
-                country = 'US'
-        else
-                country = 'TW'
-        end
-end
 
 if interface24 then
         channel24 = uci:get('wireless', interface24, 'channel')
@@ -75,9 +65,20 @@ if interface24 then
         end
 end
 
+if interface50 then
+        channel50 = uci:get('wireless', interface50, 'channel')
+        if channel50 == '32' or channel50 == '68' or channel50 == '169' or channel50 == '173' or channel50 == 'auto' or uci:get('gluon', 'wireless', 'outdoor') == '1' then
+                country = 'DE'
+        elseif channel50 == '138' or channel50 == '142' or channel50 == '144'  then
+                country = 'US'
+        else
+                country = 'TW'
+        end
+end
+
 if interface24 and interface50 then
         if channel24 == '13' then
-                if channel50 == '32' or channel50 == '34' or channel50 == '68' or channel50 == '96' or channel50 == '138' or channel50 == '142' or channel50 == '144' or channel50 == '149' or channel50 == '151' or channel50 == '153' or channel50 == '155' or channel50 == '157' or channel50 == '159' or channel50 == '161' or channel50 == '165' or channel50 == '169' or channel50 == '173' then
+                if channel50 == '32' or channel50 == '34' or channel50 == '68' or channel50 == '96' or channel50 == '138' or channel50 == '142' or channel50 == '144' or channel50 == '149' or channel50 == '151' or channel50 == '153' or channel50 == '155' or channel50 == '157' or channel50 == '159' or channel50 == '161' or channel50 == '165' or channel50 == '169' or channel50 == '173' or channel50 == 'auto' then
                         country = 'DE'
                 end
         end
@@ -116,32 +117,37 @@ end
 --- 5G
 if interface50 then
         if interface50 == 'radio0' or interface50 == 'radio1' then
-                uci:set('wireless', interface50, 'country', country)
-                VHT = cmd('iwinfo ' .. interface50 .. ' htmodelist|xargs -n 1|sort|tail -n1|tr -d "\n"')
-                if string.match(VHT, 'HT') then
-                        uci:set('wireless', interface50, 'htmode', VHT)
-                end
-                --- set values (1st pass)
-                uci:save('wireless')
-                uci:commit('wireless')
-                t = cmd('sleep 1')
-                t = cmd('/sbin/wifi')
-                t = cmd('sleep 10')
-                --- get maximum available power and step
-                t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | tail -n 2 | head -n 1 | awk \'{print $1}\'')
-                maximumTxPowerDb = string.gsub(t, "\n", "")
-                maximumTxPowerDb = tonumber(maximumTxPowerDb)
-                if maximumTxPowerDb < 30 then
-                        t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | wc -l')
-                        maximumTxPower = string.gsub(t, "\n", "")
-                        maximumTxPower = tonumber(maximumTxPower)-1
-                else
-                        t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | grep -n "19 dBm" | cut -f1 -d\':\'')
-                        maximumTxPower = string.gsub(t, "\n", "")
-                        maximumTxPower = tonumber(maximumTxPower)-1
-                end
-                --- set values (2nd pass)
-                uci:set('wireless', interface50, 'txpower', maximumTxPower)
+		if channel50 ~= 'auto' and uci:get('gluon', 'wireless', 'outdoor') ~= '1' then  --- do't do if outdoor is enabled
+	                uci:set('wireless', interface50, 'country', country)
+        	        VHT = cmd('iwinfo ' .. interface50 .. ' htmodelist|xargs -n 1|sort|tail -n1|tr -d "\n"')
+	                if string.match(VHT, 'HT') then
+        	                uci:set('wireless', interface50, 'htmode', VHT)
+                	end
+			--- set values (1st pass)
+       	        	uci:save('wireless')
+               		uci:commit('wireless')
+                	t = cmd('sleep 1')
+       	        	t = cmd('/sbin/wifi')
+              		t = cmd('sleep 10')
+                	--- get maximum available power and step
+                	t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | tail -n 2 | head -n 1 | awk \'{print $1}\'')
+                	maximumTxPowerDb = string.gsub(t, "\n", "")
+                	maximumTxPowerDb = tonumber(maximumTxPowerDb)
+                	if maximumTxPowerDb < 30 then
+                        	t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | wc -l')
+                        	maximumTxPower = string.gsub(t, "\n", "")
+                        	maximumTxPower = tonumber(maximumTxPower)-1
+                	else
+                        	t = cmd('iwinfo ' .. interface50 .. ' txpowerlist | grep -n "19 dBm" | cut -f1 -d\':\'')
+       	                	maximumTxPower = string.gsub(t, "\n", "")
+       	                	maximumTxPower = tonumber(maximumTxPower)-1
+       	        	end
+       	        	--- set values (2nd pass)
+	                uci:set('wireless', interface50, 'txpower', maximumTxPower)
+                else -- outdoors
+        	        uci:set('wireless', interface50, 'country', country)
+	               	uci:delete('wireless', interface50, 'txpower')
+		end 
                 uci:save('wireless')
                 uci:commit('wireless')
         end
