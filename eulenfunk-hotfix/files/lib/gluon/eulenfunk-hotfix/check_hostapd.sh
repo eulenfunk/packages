0de86fae15daca84d4897023e0a04410c1ba0ec4
Adorfer <adorferen@gmail.com>
2020-07-15 22:34:49 +0200

    fixes by bergi@ffms

--- eulenfunk-hotfix/files/lib/gluon/eulenfunk-hotfix/check_hostapd.sh
+++ eulenfunk-hotfix/files/lib/gluon/eulenfunk-hotfix/check_hostapd.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # check_hostapd for matching pids
-
 restart_wifi() {
   wifi down
   killall hostapd 2>/dev/null
@@ -9,41 +8,45 @@ restart_wifi() {
   wifi up
 }
 
-pspid=$1
+pspid="$1"
 phy=$(echo $@|sed 's/.*-B\ //g'|cut -d" " -f1|sed 's/.*hostapd-//g'|cut -d"." -f1)
-if [[ $(echo $phy|grep 'phy') ]] ; then # extrem schlechter Stil, leider kein =~
+if [ ${phy:0:3} = "phy" ] ; then
   pidfile=$(echo $@|sed 's/.*-P\ //g'|cut -d" " -f1)
   pid=$(cat $pidfile 2>/dev/null)
   sema="/tmp/hostapdpid"
-  if [[ $pid == $pspid ]] ; then
+  if [ "$pid" = "${pspid%% *}" ] ; then
+    rm -f $sema.fail.$phy.* 2>/dev/null
     touch $sema.ok.$phy
-    rm -f $sema.fail.$phy.* 2>/dev/null
-   else
-#   elif [ -f $sema.fail.$phy.2 ] ; then
-    logger -s -t "eulenfunk-healthcheck" "hostapd restart due to nonmatchings pids on $phy"
-    restart_wifi
-    rm -f $sema.fail.$phy.* 2>/dev/null
-    sleep 10
-#   elif [ -f $sema.fail.$phy.1 ] ; then
-#     touch $sema.fail.$phy.2
-#   else
-#    touch $sema.fail.$phy.1
-   fi
-  wifistatus=$(wifi status
+  else
+    touch $sema.fail.$phy
+    sleep 20
+    pspid=$(ps|grep hostapd|grep $phy)
+    pid=$(cat $pidfile 2>/dev/null)
+    if [ "$pid" = "${pspid%% *}" ] ; then
+      logger -s -t "eulenfunk-healthcheck" "hostapd restart due to nonmatchings pids on $phy"
+      restart_wifi
+      rm -f $sema.fail.$phy.* 2>/dev/null
+      rm -f $sema.ok.$phy.* 2>/dev/null
+      sleep 10
+    fi
+  fi
+  wifistatus=$(wifi status)
   radio="radio"${phy:3:1}
   sema="/tmp/wifipending"
-  if [ $(echo $wifistatus|grep -A 6 $radio|cut -d":" -f1-10|grep -c "up: false") -eq 1 && $(echo $wifistatus|grep -A 6 $radio|cut -d":" -f1-10|grep -c "pending: true") -eq 1 ]] ; then
-    if [ -f $sema.$radio.2 ] ; then
-      logger -s -t "eulenfunk-healthcheck" "hostapd down and pending on $radio"
-      restart_wifi
-      rm -f $sema.$phy.* 2>/dev/null
-      sleep 10
-     elif [ -f $sema.$radio.1 ] ; then
-      touch $sema.$radio.2
-     else
-      touch $sema.$radio.1
-     fi
+  if [ $(echo $wifistatus|grep -A 6 $radio|cut -d":" -f1-10|grep -c "up: false") -eq 1 ] ; then 
+    if [ $(echo $wifistatus|grep -A 6 $radio|cut -d":" -f1-10|grep -c "pending: true") -eq 1 ] ; then
+      if [ -f $sema.$radio.2 ] ; then
+        logger -s -t "eulenfunk-healthcheck" "hostapd down and pending on $radio"
+        restart_wifi
+        rm -f $sema.$phy.* 2>/dev/null
+        sleep 10
+      elif [ -f $sema.$radio.1 ] ; then
+        touch $sema.$radio.2
+      else
+        touch $sema.$radio.1
+      fi 
     else
-     rm -f $sema.$phy.* 2>/dev/null
-   fi
- fi
+      rm -f $sema.$phy.* 2>/dev/null
+    fi
+  fi
+fi
