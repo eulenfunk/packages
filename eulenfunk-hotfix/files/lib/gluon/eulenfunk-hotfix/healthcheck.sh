1614cc95fad77d293177747355ccb090815e9b2c
Adorfer <adorferen@gmail.com>
2022-07-22 01:35:36 +0200

    limit tunneldigger-threshold

--- eulenfunk-hotfix/files/lib/gluon/eulenfunk-hotfix/healthcheck.sh
+++ eulenfunk-hotfix/files/lib/gluon/eulenfunk-hotfix/healthcheck.sh
@@ -74,7 +74,7 @@ if [ $(logread -l 5|grep -c  "daemon.warn hostapd: Failed to check if DFS is req
 
 
 # too many tunneldigger restarts
-[ "$(ps |grep -c -e tunneldigger\ restart -e tunneldigger-watchdog)" -ge "9" ] && now_reboot "too many Tunneldigger-Restarts"
+[ "$(ps |grep -c -e tunneldigger\ restart -e tunneldigger-watchdog)" -ge "4" ] && now_reboot "too many Tunneldigger-Restarts"
 
 # br-client without ipv6 in prefix-range
 if [ "$(ip -6 addr show to "$(jsonfilter -i /lib/gluon/site.json -e '$.prefix6')" dev br-client | grep -c inet6)" == "0" ]; then
