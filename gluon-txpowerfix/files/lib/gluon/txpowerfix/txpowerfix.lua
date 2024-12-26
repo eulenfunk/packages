1445c7955a66a725b7b4bd4bb4cda33358c59a4f
Adorfer <adorferen@gmail.com>
2024-12-26 04:01:38 +0100

    back to HT20 for 2.4GHz (airtime issues)

--- gluon-txpowerfix/files/lib/gluon/txpowerfix/txpowerfix.lua
+++ gluon-txpowerfix/files/lib/gluon/txpowerfix/txpowerfix.lua
@@ -95,7 +95,8 @@ if interface24 then
 	uci:commit('wireless')
 	t = cmd('/sbin/wifi reconf')
 	uci:set('wireless', interface24, 'htmode', 'HT20')
-	VHT = cmd('iwinfo ' .. interface24 .. ' htmodelist|xargs -n1|grep -v "+"|grep -e "80\\|40"|tail -n1|tr -d "\n"')
+---	VHT = cmd('iwinfo ' .. interface24 .. ' htmodelist|xargs -n1|grep -v "+"|grep -e "80\\|40"|tail -n1|tr -d "\n"')
+	VHT = 'HT20' 
 	if string.match(VHT,'HT') or string.match(VHT,'HE') then
 		uci:set('wireless', interface24, 'htmode', VHT)
         end
